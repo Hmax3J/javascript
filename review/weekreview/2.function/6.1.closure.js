99783763cfeee36030d1ebea03f2953e9ed91d1f
임제훈 <Hmax3J@gmail.com>
2022-08-15 17:46:12 +0900

    2022.08.14

---
+++ review/weekreview/2.function/6.1.closure.js
@@ -0,0 +1,19 @@
+const age = 16
+console.log(sayAge)
+
+{
+    let age = 10
+
+    function sayAge() {
+        console.log(age)
+    }
+
+    let tellAge = function() {
+        console.log(age)
+    }
+
+    sayAge()
+    tellAge()
+}
+
+sayAge()
---
+++ review/weekreview/2.function/home/3.jajangmyeon.js
@@ -0,0 +1,9 @@
+chef1 = food => `달콤한 ${food}.\n`
+
+chef2 = food => `매운 ${food}.\n`
+
+waiter = (food, chef) => chef(food)
+
+console.log(waiter('짜장면', chef1),
+            waiter('짜장면', chef2),
+            waiter('짬뽕', chef2))
