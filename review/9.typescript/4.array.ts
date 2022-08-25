5857b8e887f50f70dcb77b6fe4ff115e32ef9f59
Hmax3J <Hmax3J@gmail.com>
2022-08-25 14:29:50 +0900

    2022.08.25

---
+++ review/9.typescript/4.array.ts
@@ -0,0 +1,9 @@
+const numbers: number[] = [1, 2, 3]
+const texts: string[] = ['a', 'b']
+
+interface Person {
+    name: string
+}
+
+const people: Person[] = [{name: 'john'}, {name: 'terry'}]
+console.log(people)
